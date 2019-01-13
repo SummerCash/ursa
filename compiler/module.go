07eea94a89f2190093248ce09cd5d920d83a6dcd
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-13 16:52:27 -0500

    Init section.

--- compiler/module.go
+++ compiler/module.go
@@ -4,5 +4,7 @@ import "github.com/SummerCash/ursa/compiler/wasm"
 
 // Module - wasm module
 type Module struct {
-	Base *wasm.Module `json:"base"` // Base parsed module
+	Base                 *wasm.Module   `json:"base"` // Base parsed module
+	FunctionNames        map[int]string // Module functions
+	DisableFloatingPoint bool           // Config to disable float ops
 }
