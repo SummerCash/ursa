163561865b868e6173f277742f9e90ed95b1a044
Mitsuko Megumi <mitsukomegumii@gmail.com>
2019-01-13 14:39:12 -0500

    Finished opcodes unit testing.

--- compiler/opcodes/opcodeutils_test.go
+++ compiler/opcodes/opcodeutils_test.go
@@ -1,1 +1,14 @@
 package opcodes
+
+import "testing"
+
+// TestString - test opcode stringer
+func TestString(t *testing.T) {
+	opcode := Opcode(byte(0)) // Init opcode
+
+	if opcode.String() != "Nop" { // Check invalid opcode
+		t.Fatal("invalid opcode string") // Panic
+	}
+
+	t.Logf("opcode string: %s", opcode.String()) // Log success
+}
