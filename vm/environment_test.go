e446523a2082997ae47a21891a86b2504131f508
Mitsuko Megumi <mitsukomegumii@gmail.com>
2019-01-13 15:40:15 -0500

    Init environment I/O unit testing.

--- vm/environment_test.go
+++ vm/environment_test.go
@@ -57,3 +57,43 @@ func TestEnvironmentFromBytes(t *testing.T) {
 
 	t.Log(env) // Log success
 }
+
+// TestWriteEnvironmentToMemory - test functionality of outbound env I/O
+func TestWriteEnvironmentToMemory(t *testing.T) {
+	env := NewEnvironment(10, 10, 10, 10, 10, 10, 10, false, true) // Init env
+
+	if env == nil || env.Bytes() == nil { // Check nil env
+		t.Fatal("nil environment configuration") // Panic
+	}
+
+	err := env.WriteToMemory() // Write env to memory
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	t.Log(err) // Log nil error to force code coverage
+}
+
+// TestReadEnvironmentFromMemory - test functionality of inbound env I/O
+func TestReadEnvironmentFromMemory(t *testing.T) {
+	env := NewEnvironment(10, 10, 10, 10, 10, 10, 10, false, true) // Init env
+
+	if env == nil || env.Bytes() == nil { // Check nil env
+		t.Fatal("nil environment configuration") // Panic
+	}
+
+	err := env.WriteToMemory() // Write env to memory
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	env, err = ReadEnvironmentFromMemory() // Read environment from memory
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	t.Log(err) // Log nil error to force code coverage
+}
