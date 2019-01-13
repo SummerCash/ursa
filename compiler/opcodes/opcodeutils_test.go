e5c7651102dffbd5fd74373bb935f48fcfcc28bc
Mitsuko Megumi <mitsukomegumii@gmail.com>
2019-01-13 14:59:33 -0500

    More opcode unit tests.

--- compiler/opcodes/opcodeutils_test.go
+++ compiler/opcodes/opcodeutils_test.go
@@ -4,11 +4,23 @@ import "testing"
 
 // TestString - test opcode stringer
 func TestString(t *testing.T) {
-	opcode := Opcode(byte(0)) // Init opcode
+	opcode := Opcode(byte(0)) // Init test opcode
 
 	if opcode.String() != "Nop" { // Check invalid opcode
 		t.Fatal("invalid opcode string") // Panic
 	}
 
-	t.Logf("opcode string: %s", opcode.String()) // Log success
+	opcode2 := Opcode(byte(1)) // Init 2nd test opcode
+
+	if opcode2.String() != "Unreachable" { // Check invalid opcode
+		t.Fatal("invalid opcode string") // Panic
+	}
+
+	opcode3 := Opcode(byte(161)) // Init 3rd test opcode
+
+	if opcode3.String() != "Unknown" { // Check invalid opcode
+		t.Fatal("invalid opcode string") // Panic
+	}
+
+	t.Logf("opcode strings: %s, %s, %s", opcode.String(), opcode2.String(), opcode3.String()) // Log success
 }
