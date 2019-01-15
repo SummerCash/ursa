e5436e8fceb0eb7705396fc9e130896937b27998
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-15 10:25:42 -0500

    Init gas counter.

---
+++ compiler/gas.go
@@ -0,0 +1,29 @@
+package compiler
+
+// InsertGasCounters - insert gas counter to given wasm
+func (c *SSAFunctionCompiler) InsertGasCounters(gp GasPolicy) {
+	cfg := c.NewCFGraph() // Init cf graph
+
+	for i, blk := range cfg.Blocks { // Iterate through blocks
+		//blk := &cfg.Blocks[i] // Get current block
+		blk = &blk // Set to block pointer
+
+		totalCost := int64(0) // Init gas buffer
+
+		for _, ins := range blk.Code { // Iterate through instructions
+			totalCost += gp.GetCost(ins.Op) // Get cost of instruction
+
+			if totalCost < 0 { // Check cost will cause overflow
+				panic("total cost overflow") // Panic with err
+			}
+		}
+
+		if totalCost != 0 { // Check total cost is not nil
+			blk.Code = append([]Instr{ // Append add_gas instruction
+				buildInstr(0, "add_gas", []int64{totalCost}, []TyValueID{}),
+			}, blk.Code...)
+		}
+	}
+
+	c.Code = cfg.ToInsSeq() // Set code with added gas instruction
+}
