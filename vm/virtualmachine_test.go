149e6dcf45a0601014c81485eca5ac5466545599
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-15 20:58:02 -0500

    Sync...

---
+++ vm/virtualmachine_test.go
@@ -0,0 +1,85 @@
+package vm
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+// testResolver - imports for test WebAssembly modules
+type testResolver struct {
+	tempRet0 int64
+}
+
+// TestNewVirtualMachine - test functionality of vm init
+func TestNewVirtualMachine(t *testing.T) {
+	env := &Environment{EnableJIT: false, DefaultMemoryPages: 128, DefaultTableSize: 65536} // Init env
+
+	if env == nil { // Check nil env
+		t.Fatal("nil environment configuration") // Panic
+	}
+
+	abs, err := filepath.Abs(filepath.FromSlash("../examples/main.wasm")) // Get absolute path to test WASM file
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	testSourceFile, err := ioutil.ReadFile(abs) // Read test WASM file
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	vm, err := NewVirtualMachine(testSourceFile, *env, new(testResolver), nil) // Init vm
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	t.Log(vm) // Log success
+}
+
+// ResolveFunc - define a set of import functions that may be called within a WebAssembly module
+func (r *testResolver) ResolveFunc(module, field string) FunctionImport {
+	fmt.Printf("Resolve func: %s %s\n", module, field)
+	switch module {
+	case "env":
+		switch field {
+		case "__life_ping":
+			return func(vm *VirtualMachine) int64 {
+				return vm.GetCurrentFrame().Locals[0] + 1
+			}
+		case "__life_log":
+			return func(vm *VirtualMachine) int64 {
+				ptr := int(uint32(vm.GetCurrentFrame().Locals[0]))
+				msgLen := int(uint32(vm.GetCurrentFrame().Locals[1]))
+				msg := vm.Memory[ptr : ptr+msgLen]
+				fmt.Printf("[app] %s\n", string(msg))
+				return 0
+			}
+
+		default:
+			panic(fmt.Errorf("unknown field: %s", field))
+		}
+	default:
+		panic(fmt.Errorf("unknown module: %s", module))
+	}
+}
+
+// ResolveGlobal - define a set of global variables for use within a WebAssembly module
+func (r *testResolver) ResolveGlobal(module, field string) int64 {
+	fmt.Printf("Resolve global: %s %s\n", module, field)
+	switch module {
+	case "env":
+		switch field {
+		case "__life_magic":
+			return 424
+		default:
+			panic(fmt.Errorf("unknown field: %s", field))
+		}
+	default:
+		panic(fmt.Errorf("unknown module: %s", module))
+	}
+}
