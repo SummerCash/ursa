1656ce2cff6fdd5f6aa7f8232877e4bfdf3a8ffe
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-17 21:07:58 -0500

    State load works!

--- vm/state_test.go
+++ vm/state_test.go
@@ -50,3 +50,58 @@ func TestSaveState(t *testing.T) {
 
 	t.Log(result) // Log success
 }
+
+// TestLoadState - test functionality of inbound state I/O
+func TestLoadState(t *testing.T) {
+	abs, err := filepath.Abs(filepath.FromSlash("../examples/main.wasm")) // Get absolute path to test WASM file
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	testSourceFile, err := ioutil.ReadFile(abs) // Read test WASM file
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	simpleGasPolicy := &compiler.SimpleGasPolicy{GasPerInstruction: 1} // Init simple gas policy
+
+	vm, err := NewVirtualMachine(testSourceFile, Environment{}, &NopResolver{}, simpleGasPolicy) // Init vm
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	entryID, ok := vm.GetFunctionExport("main") // Get main func
+
+	if ok != true { // Check for errors
+		t.Fatal(entryID) // Panic
+	}
+
+	_, err = vm.Run(entryID) // Execute
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	err = vm.SaveState() // Save VM state
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	loadVM, err := NewVirtualMachine(testSourceFile, Environment{}, &NopResolver{}, simpleGasPolicy) // Init vm
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	err = loadVM.LoadState() // Load state
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	t.Log(loadVM.ReturnValue) // Log result
+}
